dc79bf08d46953e73a3d4c49c2a41017226714fb
SteelRobot <alutchak@yandex.ru>
2023-05-25 22:54:06 +0300

    Test Fix

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,15 +2,29 @@ import org.enemies.Golem;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.main.Game;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 public class AppTest {
-    private final Game game = new Game();
+    private Game game;
+
     @Before
     public void setUpHeadlessMode() {
         System.setProperty("java.awt.headless", "true");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            game = new Game();
+        } catch (HeadlessException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
@@ -28,6 +42,7 @@ public class AppTest {
         i = game.getPlaying().getEnemyManager().getAmountOfAllLiveEnemies();
         assertEquals(0, i);
     }
+
     @org.junit.jupiter.api.Test
     void hurtEnemyUntilDead2() {
         Golem golem = new Golem(0, 0, 0, game.getPlaying().getEnemyManager());
